44a8ea6de09f4ea73a7702b663ff0d54ad47279a
mohamed ali <mm15718.mm@gmail.com>
2025-10-04 02:11:28 +0300

    fixed 5

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3268,11 +3268,6 @@ const ReportManager = {
                         </div>
                         <div class="report-actions" onclick="event.stopPropagation()">
                             ${!isInactive ? `
-                                <button class="btn-icon-stylish view-btn" onclick="ReportManager.viewReport(${report.id})" title="${currentLanguage === 'ar' ? 'عرض التقرير' : 'View Report'}">
-                                    <svg viewBox="0 0 24 24" width="16" height="16">
-                                        <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
-                                    </svg>
-                                </button>
                                 <button class="btn-icon-stylish print-btn" onclick="ReportManager.printReport(${report.id})" title="${currentLanguage === 'ar' ? 'طباعة' : 'Print'}">
                                     <svg viewBox="0 0 24 24" width="16" height="16">
                                         <path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z"/>
